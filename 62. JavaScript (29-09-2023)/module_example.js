cd5c9e3779ea858c6211ae62b33e8f332e4fa5a1
Marco Malchiodi <140761047+MarcoMalchiodi@users.noreply.github.com>
2023-12-15 11:41:30 +0200

    Add files via upload

---
+++ 62. JavaScript (29-09-2023)/module_example.js
@@ -0,0 +1,12 @@
+const myName = (name) => 'Hi! My name is ' + name;
+
+const functionOne = () => 'ONE';
+const functionTwo = () => 'TWO';
+
+
+export default myName;
+
+export {
+    functionOne,
+    functionTwo
+  };
