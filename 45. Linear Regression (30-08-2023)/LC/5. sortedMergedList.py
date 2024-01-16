7bb869d8e7f8757db32ed67ace77493397f59cf1
Marco Malchiodi <140761047+MarcoMalchiodi@users.noreply.github.com>
2024-01-16 17:45:21 +0200

    Add files via upload

---
+++ 45. Linear Regression (30-08-2023)/LC/5. sortedMergedList.py
@@ -0,0 +1,9 @@
+def sortMergeList(list_1,list_2):
+    temp_list = list_1 + list_2
+    temp_list = sorted(temp_list)
+    
+    return print(temp_list)
+    
+    
+    
+sortMergeList([1,4,6],[2,5,3])
